e195ab063a3dfbfcce598531cced7a7adb3e35ab
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2021-12-16 14:52:28 -0500

    service worker debug

--- src/hooks/useServiceWorker.js
+++ src/hooks/useServiceWorker.js
@@ -1,13 +1,34 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
 const useServiceWorker = () => {
   const [updateNeeded, setUpdateNeeded] = useState(false);
-  const refreshing = useRef(false);
+  //const refreshing = useRef(false);
 
   useEffect(() => {
     if ('serviceWorker' in navigator) {
       console.log('in nav');
+      navigator.serviceWorker.getRegistration().then((registration) => {
+        console.log('registrration', registration);
+        registration.addEventListener('onupdatefound', () => {
+          console.log('onupdatefound');
+          const installingWorker = registration.installing;
+          if (installingWorker) {
+            console.log('installingWorker', installingWorker);
+            installingWorker.onstatechange = () => {
+              console.log('onstatechange', installingWorker.state);
+              if (installingWorker.state === 'installed') {
+                console.log('installed');
+                if (navigator.serviceWorker.controller) {
+                  console.log('ready to reload');
+                  setUpdateNeeded(true);
+                }
+              }
+            };
+          }
+        });
+      });
 
+      /*
       navigator.serviceWorker.addEventListener('controllerchange', () => {
         console.log('controllerchange');
         if (refreshing.current) return;
@@ -40,6 +61,7 @@ const useServiceWorker = () => {
         // if nothing new yet, listen for updatefound, then wait for install
         reg.addEventListener('updatefound', waitForInstall);
       });
+      */
     }
 
   // eslint-disable-next-line react-hooks/exhaustive-deps
@@ -50,10 +72,21 @@ const useServiceWorker = () => {
     onUpdateVersion: useCallback(() => {
       console.log('onUpdateVersion');
       if ('serviceWorker' in navigator) {
+        navigator.serviceWorker.getRegistration().then((registration) => {
+          console.log('reg', registration);
+          console.log('waiting', registration.waiting);
+          console.log('installing', registration.installing);
+          console.log('installed', registration.installed);
+          registration.waiting.postMessage({ type: 'SKIP_WAITING' });
+        });
+      }
+      /*
+      console.log('onUpdateVersion');
+      if ('serviceWorker' in navigator) {
         navigator.serviceWorker.getRegistration().then((reg) => {
           if (reg) {
             console.log('skipWaiting');
-            reg.waiting.postMessage({type: 'SKIP_WAITING'});
+            reg.waiting.postMessage({ type: 'SKIP_WAITING' });
           } else {
             console.log('reload 1');
             window.location.reload();
@@ -63,6 +96,7 @@ const useServiceWorker = () => {
         console.log('reload 2');
         window.location.reload();
       }
+      */
     }, [])
   }
 };
