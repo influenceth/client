c58ee0256019d8ecbe18cbbd4055883da20add35
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2022-05-06 13:59:49 -0400

    use mouse events for draggable window

--- src/components/DraggableModal.js
+++ src/components/DraggableModal.js
@@ -1,20 +1,23 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import styled from 'styled-components';
 
 import IconButton from '~/components/IconButton';
 import { CloseIcon } from '~/components/Icons';
 import useStore from '~/hooks/useStore';
 
-const Wrapper = styled.div`
-  left: ${p => p.position?.x || 0}%;
-  opacity: ${p => p.isDragging ? '0' : '1'};
+const Wrapper = styled.div.attrs(props => ({
+  style: {
+    left: `${props.position?.x > -1 ? props.position.x : 2}%`,
+    top: `${props.position?.y > -1 ? props.position.y : 2}%`,
+  },
+}))`
   overflow: hidden;
   position: fixed;
-  top: ${p => p.position?.y || 0}%;
   z-index: ${p => (p.draggableIndex || 0) + 100};
 
   @media (max-width: ${p => p.theme.breakpoints.mobile}px) {
-    /* TODO: */
+    left: 0 !important;
+    top: 0 !important;
   }
 `;
 
@@ -47,6 +50,7 @@ const Header = styled.div`
   display: flex;
   flex-direction: row;
   height: ${headerHeight}px;
+  justify-content: space-between;
   padding: 0 0 0 20px;
   position: relative;
   z-index: 1;
@@ -58,7 +62,6 @@ const Header = styled.div`
 `;
 
 const Title = styled.h1`
-  flex: 1;
   font-size: 20px;
   font-weight: 400;
   line-height: ${headerHeight}px;
@@ -96,12 +99,7 @@ const Content = styled.div`
 `;
 
 const undraggable = {
-  draggable: true,
-  onDragStart: (e) => {
-    e.preventDefault();
-    e.stopPropagation();
-    return false;
-  }
+  onMouseDown: (e) => e.stopPropagation()
 };
 
 // TODO (enhancement): on resize, make sure dialogs still entirely on screen
@@ -112,53 +110,64 @@ const DraggableModal = ({ draggableId, ...props }) => {
   const dispatchDraggableMoved = useStore(s => s.dispatchDraggableMoved);
 
   const [draggingFrom, setDraggingFrom] = useState();
+  const [tmpPosition, setTmpPosition] = useState(false);
 
-  const handleDragStart = useCallback((e) => {
-    setTimeout(() => {  // weirdness if alter target within dragstart event
-      setDraggingFrom({
-        x: e.nativeEvent.offsetX,
-        y: e.nativeEvent.offsetY
-      });
-    }, 0);
-  }, []);
-
-  const handleDragEnd = useCallback((e) => {
-    // constrain to page, convert to % in case of screen resize
-    dispatchDraggableMoved(draggableId, {
-      x: 100 * Math.min(Math.max(0, e.nativeEvent.x - draggingFrom.x), window.innerWidth - e.target.clientWidth) / window.innerWidth,
-      y: 100 * Math.min(Math.max(0, e.nativeEvent.y - draggingFrom.y), window.innerHeight - e.target.clientHeight) / window.innerHeight,
-    });
-    // TODO: this causes race condition b/w local and app-level states (which
-    //  sometimes makes modal reappear in old position before jumping to new)
-    setDraggingFrom();
-  }, [dispatchDraggableMoved, draggableId, draggingFrom]);
+  const draggable = useRef();
 
   const handleMouseDown = useCallback((e) => {
     dispatchDraggableToFront(draggableId);
+    setDraggingFrom({
+      x: e.nativeEvent.offsetX,
+      y: e.nativeEvent.offsetY
+    });
   }, [dispatchDraggableToFront, draggableId]);
 
-  const { title, contentProps = {}, draggableData, ...restProps } = props;
+  const handleMouseUp = useCallback(() => {
+    if (!!draggingFrom) {
+      setDraggingFrom();
+      if (tmpPosition) {
+        dispatchDraggableMoved(draggableId, tmpPosition);
+        setTmpPosition();
+      }
+    }
+  }, [dispatchDraggableMoved, draggingFrom, draggableId, tmpPosition]);
 
-  // TODO: on some mobile devices, might be good to actually disable draggable / drag events,
+  useEffect(() => {
+    if (!!draggingFrom) {
+      document.onmousemove = (e) => {
+        setTmpPosition({
+          x: 100 * Math.min(Math.max(0, e.x - draggingFrom.x), window.innerWidth - draggable.current.clientWidth) / window.innerWidth,
+          y: 100 * Math.min(Math.max(0, e.y - draggingFrom.y), window.innerHeight - draggable.current.clientHeight) / window.innerHeight,
+        });
+      };
+    } else {
+      document.onmousemove = undefined;
+    }
+    return () => {
+      document.onmousemove = undefined;
+    };
+  }, [draggingFrom]);
+
+  // TODO (enhancement): on some mobile devices, might be good to actually disable draggable / drag events,
   //  but doesn't actually seem possible to drag on mobile emulator at least
+  const { title, contentProps = {}, draggableData, ...restProps } = props;
   return (
     <Wrapper
-      draggable
-      onDragStart={handleDragStart}
-      onDragEnd={handleDragEnd}
-      onMouseDown={handleMouseDown}
-      isDragging={!!draggingFrom}
-      position={position}
       draggableIndex={draggableIndex}
+      isDragging={!!draggingFrom}
+      position={tmpPosition || position}
+      ref={draggable}
       {...restProps}>
       <Container {...restProps}>
-        <Header>
-          {title && <Title>{title}</Title>}
-          <CloseButton onClick={() => dispatchDraggableClose(draggableId)} {...undraggable}>
+        <Header
+          onMouseDown={handleMouseDown}
+          onMouseUp={handleMouseUp}>
+          {title && <Title {...undraggable}>{title}</Title>}
+          <CloseButton {...undraggable} onClick={() => dispatchDraggableClose(draggableId)}>
             <CloseIcon />
           </CloseButton>
         </Header>
-        <Content {...contentProps} {...undraggable}>
+        <Content {...contentProps}>
           {props.children}
         </Content>
       </Container>
