14793c47a3e5230bf1d0e3660f08440106784e40
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2022-10-05 14:28:34 -0400

    asteroid overlay text

--- src/game/interface/details/asteroidDetails/AsteroidGraphic.js
+++ src/game/interface/details/asteroidDetails/AsteroidGraphic.js
@@ -1,13 +1,16 @@
 import { useCallback, useState } from 'react';
-import styled from 'styled-components';
+import styled, { css, keyframes } from 'styled-components';
 import { Canvas } from '@react-three/fiber';
 import utils from 'influence-utils';
 import { sRGBEncoding } from 'three';
 
+import useStore from '~/hooks/useStore';
 import useWebWorker from '~/hooks/useWebWorker';
 import AsteroidComposition from './AsteroidComposition';
 import AsteroidRendering from './AsteroidRendering';
 import AsteroidSpinner from './AsteroidSpinner';
+import { WarningOutlineIcon } from '~/components/Icons';
+import theme from '~/theme';
 
 const OPACITY_ANIMATION = 400;
 
@@ -56,11 +59,37 @@ const AsteroidName = styled.div`
   padding: 4px 12px;
   text-align: center;
   text-transform: none !important;
-  min-width: 60%;
+  min-width: 55%;
+`;
+
+const opacityKeyframes = keyframes`
+  0% {
+    opacity: 0.3;
+  }
+  50% {
+    opacity: 1;
+  }
+  100% {
+    opacity: 0.3;
+  }
+`;
+const opacityAnimation = css`
+  animation: ${opacityKeyframes} 1200ms ease-in-out infinite;
+`;
+const LastRow = styled.div`
+  ${p => p.animate && opacityAnimation};
+  text-align: center;
+  text-transform: uppercase;
+  & > svg {
+    font-size: 36px;
+    margin-bottom: -18px;
+  }
 `;
 
 const AsteroidGraphic = ({ asteroid, ...compositionProps }) => {
   const webWorkerPool = useWebWorker();
+
+  const saleIsActive = useStore(s => s.sale);
 
   const [frameloop, setFrameloop] = useState();
   const [ready, setReady] = useState();
@@ -106,9 +135,37 @@ const AsteroidGraphic = ({ asteroid, ...compositionProps }) => {
         <AsteroidName>
           {asteroid.customName ? `\`${asteroid.customName}\`` : asteroid.baseName}
         </AsteroidName>
-        <div>
-          {Number(Math.floor(4 * Math.PI * Math.pow(asteroid.radius / 1000, 2))).toLocaleString()} lots
-        </div>
+        {asteroid.scanned && (
+          <LastRow style={{ color: 'white' }}>
+            {Number(Math.floor(4 * Math.PI * Math.pow(asteroid.radius / 1000, 2))).toLocaleString()} lots
+          </LastRow>
+        )}
+        {!asteroid.scanned && asteroid.scanStatus === 'SCANNING' && (
+          <LastRow animate style={{ color: theme.colors.main, fontSize: '85%', fontWeight: 'bold' }}>
+            Analyzing<br/>Resource Matrix...
+          </LastRow>
+        )}
+        {!asteroid.scanned && asteroid.scanStatus === 'SCAN_READY' && (
+          <LastRow style={{ color: theme.colors.success, fontSize: '85%', fontWeight: 'bold' }}>
+            Resource<br/>Analysis Ready
+          </LastRow>
+        )}
+        {!asteroid.scanned && asteroid.scanStatus === 'UNSCANNED' && asteroid.owner && (
+          <LastRow style={{ color: theme.colors.error }}>
+            Un-Scanned<br/>
+            <WarningOutlineIcon />
+          </LastRow>
+        )}
+        {!asteroid.scanned && asteroid.scanStatus === 'UNSCANNED' && !asteroid.owner && saleIsActive && (
+          <LastRow style={{ color: '#55d0fa', fontWeight: 'bold' }}>
+            Available<br/>for Purchase
+          </LastRow>
+        )}
+        {!asteroid.scanned && asteroid.scanStatus === 'UNSCANNED' && !asteroid.owner && !saleIsActive && (
+          <LastRow>
+            Unavailable
+          </LastRow>
+        )}
       </GraphicData>
     </Graphic>
   );
