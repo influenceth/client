62ff2391a4326c784ae78c9166335ce27e3af8d5
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2023-10-05 14:01:43 -0400

    bump

--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -10,6 +10,8 @@
 // To learn more about the benefits of this model and instructions on how to
 // opt-in, read https://cra.link/PWA
 
+console.log('SW deploy 2');
+
 const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
