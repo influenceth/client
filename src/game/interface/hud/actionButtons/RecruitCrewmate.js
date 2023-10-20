5bf078bf2306eaf06dfb3c5db779008c8b22b31e
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2023-10-20 12:30:05 -0400

    handle more cases in actionbutton for crewmate recruitment

--- src/game/interface/hud/actionButtons/RecruitCrewmate.js
+++ src/game/interface/hud/actionButtons/RecruitCrewmate.js
@@ -3,25 +3,64 @@ import { useHistory } from 'react-router-dom';
 
 import { RecruitCrewmateIcon } from '~/components/Icons';  // TODO: sergey's
 import useCrewManager from '~/hooks/useCrewManager';
-import ActionButton from './ActionButton';
+import useStore from '~/hooks/useStore';
 import { nativeBool, reactBool } from '~/lib/utils';
+import ActionButton from './ActionButton';
 
-const RecruitCrewmate = ({ crew, lot }) => {
+const RecruitCrewmate = ({ crew, lot }) => {  
   const { getPendingCrewmate } = useCrewManager();
   const history = useHistory();
 
+  const createAlert = useStore(s => s.dispatchAlertLogged);
+
+  const [tooltipExtra, recruitToCrew, clickWarning] = useMemo(() => {
+    let tip = '';
+    let ontoCrew = 0;
+    let warning = '';
+
+    // if active crew is stationed here...
+    if (crew?._location?.buildingId === lot?.building?.id) {
+      // ... and if crew has space, add to crew
+      if (crew.Crew?.roster?.length < 5) {
+        ontoCrew = crew.id;
+
+      // ... else if crew is full, start new crew (with alert warning)
+      } else {
+        tip += ` (Start New Crew)`;
+        warning = `Because your active crew's roster is full, this crewmate will be recruited to a new crew.`;
+      }
+
+    // else if no crew or crew is stationed elsewhere, will start a new crew (only warn for latter)
+    } else {
+      tip += ` (Start New Crew)`;
+      if (crew) {
+        warning = 'Because your active crew is stationed elsewhere, this crewmate will be recruited to a new crew.';
+      }
+    }
+
+    return [tip, ontoCrew, warning];
+  }, [crew, lot]);
+
+
   const handleClick = useCallback(() => {
-    history.push(`/recruit/${crew?.id || 0}/${lot?.building?.id}`)
-  }, [crew?.id, lot?.building?.id]);
+    if (clickWarning) {
+      createAlert({
+        type: 'GenericAlert',
+        data: { content: clickWarning },
+        duration: 6000
+      });
+    }
+    history.push(`/recruit/${recruitToCrew || 0}/${lot?.building?.id}`)
+  }, [recruitToCrew, clickWarning, lot?.building?.id]);
 
   const pendingCrewmate = useMemo(getPendingCrewmate, [getPendingCrewmate]);
 
   return (
     <ActionButton
-      label={`Recruit Crewmate${crew?.Crew?.roster?.length >= 5 ? ' (Crew is full)' : ''}`}
+      label={`Recruit Crewmate${tooltipExtra}`}
       flags={{
         attention: reactBool(!pendingCrewmate && !(crew?.Crew?.roster?.length > 0)),
-        disabled: nativeBool(!!pendingCrewmate || crew?.Crew?.roster?.length >= 5),
+        disabled: nativeBool(!!pendingCrewmate),
         loading: reactBool(!!pendingCrewmate),
       }}
       icon={<RecruitCrewmateIcon />}
