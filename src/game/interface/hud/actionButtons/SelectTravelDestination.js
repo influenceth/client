a799d9c425fb867c1140cdcaa476110e0ef4aa7d
Chris Lexmond <chris@chrislexmond.com>
2024-02-20 22:46:58 -0500

    Fix flight planning not displaying

--- src/game/interface/hud/actionButtons/SelectTravelDestination.js
+++ src/game/interface/hud/actionButtons/SelectTravelDestination.js
@@ -6,6 +6,8 @@ import ActionButton from './ActionButton';
 import useShipTravelManager from '~/hooks/actionManagers/useShipTravelManager';
 
 const isVisible = ({ asteroid, crew, ship, zoomStatus }) => {
+  if (!ship && crew?.Ship?.emergencyAt > 0) return true; // crew is in escape module
+
   return crew && (
     (asteroid && zoomStatus === 'out') || (
       ship
