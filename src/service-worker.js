50b8442277568168d8a20f65f3b54535c7b72fdc
Chris Lexmond <chris@chrislexmond.com>
2024-03-20 13:30:38 +0100

    Revert "Trying something else for caching"
    This reverts commit 45104decda8f5b8f85d61cb98a69ba29a72c8967.

--- src/service-worker.js
+++ src/service-worker.js
@@ -20,11 +20,10 @@ clientsClaim();
 // Their URLs are injected into the manifest variable below.
 // This variable must be present somewhere in your service worker file,
 // even if you decide not to use precaching. See https://cra.link/PWA
-precacheAndRoute(
-  self.__WB_MANIFEST
-    .concat(getBuildingModels().map(m => { return { url: m, revision: null }; }))
-    .concat(getShipModels().map(m => { return { url: m, revision: null }; }))
-);
+let manifestRoutes = self.__WB_MANIFEST;
+manifestRoutes = manifestRoutes.concat(getBuildingModels().map(m => { return { url: m, revision: null }; }));
+manifestRoutes = manifestRoutes.concat(getShipModels().map(m => { return { url: m, revision: null }; }));
+precacheAndRoute(manifestRoutes);
 
 // Set up App Shell-style routing, so that all navigation requests
 // are fulfilled with your index.html shell. Learn more at
