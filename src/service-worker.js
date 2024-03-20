9847f192311c189b834797c11895966b8782c475
Chris Lexmond <chris@chrislexmond.com>
2024-03-20 12:53:14 +0100

    Forgot to include import

--- src/service-worker.js
+++ src/service-worker.js
@@ -9,7 +9,7 @@
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
-import { precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
+import { precache, precacheAndRoute, createHandlerBoundToURL } from 'workbox-precaching';
 import { registerRoute } from 'workbox-routing';
 import { StaleWhileRevalidate } from 'workbox-strategies';
 import { getBuildingModels, getShipModels } from './lib/assetUtils';
