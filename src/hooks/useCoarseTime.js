fda15f5c9b595a380cdc6cf71b27b2fcf83e3520
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-03-21 14:23:47 -0400

    more clock cleanup

--- src/hooks/useCoarseTime.js
+++ src/hooks/useCoarseTime.js
@@ -8,6 +8,7 @@ import useStore from '~/hooks/useStore';
 import { displayTimeFractionDigits } from '~/lib/utils';
 
 const defaultDenom = Math.pow(10, displayTimeFractionDigits);
+const minInterval = 1000 / 30; // no reason to update more often than FPS
 
 const formatAsCoarseTime = (preciseTime, denom) => {
   return Math.floor(denom * preciseTime) / denom;
@@ -28,8 +29,8 @@ const useCoarseTime = (denom = defaultDenom) => {
       if (timeOverride?.speed === 0) return null; // (tells useInterval to not run if clock paused)
 
       return Math.floor(Math.max(
-        1000 / 30, // no reason to update more often than FPS
-        Time.toRealDuration(86400e3 / denom, TIME_ACCELERATION || Time.DEFAULT_TIME_ACCELERATION) / Math.abs(timeOverride?.speed || 1)
+        minInterval,
+        Time.toRealDuration(86400e3 / denom, TIME_ACCELERATION) / Math.abs(timeOverride?.speed || 1)
       ));
     },
     [denom, TIME_ACCELERATION, timeOverride?.speed]
@@ -37,20 +38,37 @@ const useCoarseTime = (denom = defaultDenom) => {
 
   // sync to nearest coarseness (so any consumers all match each other in display)
   useEffect(() => {
+    const currentTime = getTime();
+    const currentCoarseTime = formatAsCoarseTime(currentTime, denom);
+    setCoarseTime(currentCoarseTime);
+    
     if (tickLength > 0) {
-      setSyncing(true);
+      // target next half-denom for start (i.e. if denom = 100, target 0.01 / 2 --> 0.005)
+      // to avoid rounding issues, since the whole point of this timeout is to sync the intervals
+      // to the same display values...
+      const modulus = denom * (currentTime - currentCoarseTime);
+      const targetStartIn = timeOverride?.speed < 0
+        ? ((modulus > 0.5 ? -0.5 : 0.5) + modulus)  // (negative speed)
+        : ((modulus > 0.5 ? 1.5 : 0.5) - modulus);  // (positive speed)
+      const msUntilTarget = Math.ceil(Time.toRealDuration(86400e3 * targetStartIn / denom, TIME_ACCELERATION) / Math.abs(timeOverride?.speed || 1));
 
-      const realTime = Time.fromOrbitADays(getTime(), TIME_ACCELERATION || Time.DEFAULT_TIME_ACCELERATION).unixTimeMS;
-      const timeUntilSync = tickLength - (realTime % tickLength) + 1;
-      
-      const to = setTimeout(() => setSyncing(false), timeUntilSync);
-      return () => clearTimeout(to);
+      if (msUntilTarget > minInterval) {
+        setSyncing(true);
+        const to = setTimeout(() => {
+          setCoarseTime(formatAsCoarseTime(getTime(), denom));
+          setSyncing(false);
+        }, msUntilTarget);
+        return () => clearTimeout(to);
+      }
+    } else {
+      setSyncing(false);
     }
-  }, [tickLength]);
+  }, [getTime, tickLength]);
 
   useInterval(() => {
     setCoarseTime(formatAsCoarseTime(getTime(), denom));
   }, syncing ? null : tickLength);
+
   return coarseTime;
 };
 
--- src/hooks/useGetTime.js
+++ src/hooks/useGetTime.js
@@ -6,20 +6,19 @@ import useStore from '~/hooks/useStore';
 
 const useGetTime = () => {
   const timeOverride = useStore(s => s.timeOverride);
-  const { data: TIME_ACCELERATION, isLoading } = useConstants('TIME_ACCELERATION');
+  const { data: TIME_ACCELERATION } = useConstants('TIME_ACCELERATION');
 
   return useCallback((overrideNow) => {
-    if (isLoading) return 0;
     const now = overrideNow || Date.now();
     let preciseTime = timeOverride?.anchor || Time.fromUnixMilliseconds(now, TIME_ACCELERATION).toOrbitADays();
 
     // console.log('override.speed', now - override.ts);
     // if (override.speed) preciseTime += override.speed * (now - override.ts) / 30;
-    if (timeOverride?.speed) preciseTime += (timeOverride.speed - 1) * (now - timeOverride.ts) / 3600e3;
+    if (timeOverride?.speed) preciseTime += (timeOverride.speed - 1) * (now - timeOverride.ts) / (1e3 * Time.getSecondsPerAday(TIME_ACCELERATION));
 
     // console.log('preciseTime', preciseTime, Math.floor(coarseTimeInterval * preciseTime) / coarseTimeInterval);
     return preciseTime;
-  }, [isLoading, timeOverride, TIME_ACCELERATION]);
+  }, [timeOverride, TIME_ACCELERATION]);
 };
 
 export default useGetTime;
