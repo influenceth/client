d265cedef341c72e6ed4e5d489db60aef102e660
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-03-21 20:52:17 -0400

    throttle planet updates

--- src/game/scene/Planets.js
+++ src/game/scene/Planets.js
@@ -17,11 +17,13 @@ const Planets = () => {
   const texture = useTexture(`${process.env.PUBLIC_URL}/textures/circleFaded.png`);
   const { processInBackground } = useWebWorker();
 
+  const isUpdating = useRef(false);
   const geometry = useRef();
 
   // Listen for changes to planets data or global time and update planet positions
   useEffect(() => {
-    if (planets && coarseTime) {
+    if (planets && coarseTime && !isUpdating.current) {
+      isUpdating.current = true;
       processInBackground(
         {
           topic: 'updatePlanetPositions',
@@ -33,6 +35,7 @@ const Planets = () => {
           _cacheable: 'planets'
         },
         (data) => {
+          isUpdating.current = false;
           if (geometry.current && data.positions) {
             geometry.current.setAttribute(
               'position',
