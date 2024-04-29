52b1b12b51a4dbba501925c4edad12476708383a
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-04-29 12:50:27 -0400

    show lot abundance to tenth of percent in action dialog

--- src/game/interface/hud/actionDialogs/NewCoreSample.js
+++ src/game/interface/hud/actionDialogs/NewCoreSample.js
@@ -257,7 +257,7 @@ const NewCoreSample = ({ asteroid, lot, coreSampleManager, stage, ...props }) =>
               disabled={stage !== actionStage.NOT_STARTED}
               sublabel={
                 resourceId
-                ? <><b style={{ color: 'white' }}>{formatFixed(100 * lotAbundance, 0)}%</b> Lot Abundance</>
+                ? <><b style={{ color: 'white' }}>{formatFixed(100 * lotAbundance, 1)}%</b> Lot Abundance</>
                 : 'Resource'
               }
             />
