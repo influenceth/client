3f777c3ab21e18df23095cd1b397630f28fc8ece
Chris Lexmond <chris@chrislexmond.com>
2024-06-14 11:53:46 +0200

    More merge conflicts / rebasing

--- src/hooks/useWalletTokenBalance.js
+++ src/hooks/useWalletTokenBalance.js
@@ -4,14 +4,8 @@ import { uint256 } from 'starknet';
 import useSession from '~/hooks/useSession';
 import { TOKEN } from '~/lib/priceUtils';
 
-<<<<<<< HEAD:src/hooks/useWalletTokenBalance.js
 const useWalletTokenBalance = (tokenLabel, tokenAddress, overrideAccount) => {
   const { accountAddress: defaultAccount, starknet } = useSession();
-=======
-const useEthBalance = (overrideAccount) => {
-  const { accountAddress: defaultAccount, provider } = useSession();
-  const accountAddress = overrideAccount || defaultAccount;
->>>>>>> fbdbd861 (More sessions work):src/hooks/useEthBalance.js
 
   const accountAddress = overrideAccount || defaultAccount;
   return useQuery(
@@ -19,13 +13,8 @@ const useEthBalance = (overrideAccount) => {
     async () => {
       if (!accountAddress) return undefined; // shouldn't happen (but seemingly does)
       try {
-<<<<<<< HEAD:src/hooks/useWalletTokenBalance.js
         const balance = await starknet.provider.callContract({
           contractAddress: tokenAddress,
-=======
-        const balance = await provider.callContract({
-          contractAddress: process.env.REACT_APP_ERC20_TOKEN_ADDRESS,
->>>>>>> fbdbd861 (More sessions work):src/hooks/useEthBalance.js
           entrypoint: 'balanceOf',
           calldata: [accountAddress]
         });
