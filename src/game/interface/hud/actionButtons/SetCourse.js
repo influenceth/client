23e977e3632bc9af2139abda8fc8e2afbff02d05
Patrick Keenum <5631043+prof197@users.noreply.github.com>
2024-07-12 10:17:45 -0400

    fix escape pod action window

--- src/game/interface/hud/actionButtons/SetCourse.js
+++ src/game/interface/hud/actionButtons/SetCourse.js
@@ -20,13 +20,14 @@ const isVisible = ({ openHudMenu, /*asteroid, crew, ship, zoomStatus*/ }) => {
 };
 
 const SetCourse = ({ asteroid, crew, ship, onSetAction, _disabled }) => {
-  const { currentTravelAction, travelStatus } = useShipTravelManager(crew?._location?.shipId);
+  const inEscapeModule = !crew?._location?.shipId && crew?.Ship?.emergencyAt > 0;
+  const { currentTravelAction, travelStatus } = useShipTravelManager(inEscapeModule ? crew : crew?._location?.shipId);
   const travelSolutionIsValid = useTravelSolutionIsValid();
   const travelSolution = useStore(s => s.asteroids.travelSolution);
   const { data: destination } = useAsteroid(travelSolution?.destinationId);
 
   const handleClick = useCallback(() => {
-    onSetAction('SET_COURSE', { travelSolution });
+    onSetAction('SET_COURSE');
   }, [travelSolution]);
 
   const validDestination = useMemo(() => {
@@ -35,13 +36,13 @@ const SetCourse = ({ asteroid, crew, ship, onSetAction, _disabled }) => {
 
   const disabledReason = useMemo(() => {
     if (_disabled) return 'loading...';
-    if (!crew?._location?.shipId && crew?.Ship?.emergencyAt === 0) return 'crew is not on ship';
+    if (!crew?._location?.shipId && !inEscapeModule) return 'crew is not on ship';
     if (travelStatus === 'READY') {
       if (!travelSolution) return 'no travel solution';
       if (travelSolution._isSimulation) return 'simulated solution';
       if (travelSolution.originId !== crew?._location?.asteroidId) return 'invalid travel origin';
       if (!travelSolutionIsValid) return 'invalid travel solution';
-      if (ship?.Ship?.transitDeparture > 0) return 'ship is in flight';
+      if ((inEscapeModule ? crew?.Ship?.transitDeparture : ship?.Ship?.transitDeparture) > 0) return 'ship is in flight';
       if (ship?._location?.lotId) return 'ship is docked';
       if (!validDestination) return 'destination not scanned';
       return getCrewDisabledReason({ crew });
